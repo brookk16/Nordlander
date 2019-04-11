f1f6282d6d15494a12f60bd2fab8837861a5ba8d
Brook Kynaston <brookkynaston@live.fr>
2019-04-11 16:33:46 +0000

    Created the models for features and bugs, and started html for features page
    both models for features and bugs have been created (bugs has one field commented out until the 'comments' app is made). Started the html styling for the features page

--- bugs/models.py
+++ bugs/models.py
@@ -1,11 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Bugs(models.Model):
+    
+    STATUSES = {
+        ("TODO", "To do"),
+        ("DOING", "Doing"),
+        ("FIXED", "Fixed!"),
+    }
+    
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='images')
+    created_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    upvotes = models.IntegerField(default=0)
+    status = models.CharField(choices=STATUSES, default="TODO", max_length=6)
+    
+    """comments = models.ForeignKey(Comment, null=False)"""
+    
+    
 
     def __str__(self):
         return self.name
--- features/models.py
+++ features/models.py
@@ -2,10 +2,27 @@ from django.db import models
 
 # Create your models here.
 class Features(models.Model):
+    
+    FEATURE_TYPES = {
+        ("ITEM", "Item"),
+        ("WORLD", "World"),
+        ("SKILLS", "Skills"),
+        ("QUESTS", "Quests")
+    }
+    
+    STATUSES = {
+        ("TODO", "To do"),
+        ("DOING", "Doing"),
+        ("AVAILABLE", "Available now!"),
+    }
+    
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
+    type = models.CharField(choices=FEATURE_TYPES, max_length=6, default="ITEM")
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
+    likes =  models.IntegerField(default=0)
+    status = models.CharField(choices=STATUSES, default="TODO", max_length=14)
 
     def __str__(self):
         return self.name
