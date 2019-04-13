fd3ddf45742de6c9d58b43ea8142c18bb4d2cc3f
Brook Kynaston <brookkynaston@live.fr>
2019-04-13 16:11:54 +0000

    Search function works for features
    search function fully works for features, users can search by name, by category or a combination of the 2

--- search/views.py
+++ search/views.py
@@ -3,7 +3,23 @@ from features.models import Features
 
 # Create your views here.
 def do_search(request):
-    features = Features.objects.filter(name__icontains=request.GET['search'])
-    return render(request, "features.html", {"features": features})
     
+    if request.method == 'GET':
+    
+        if request.GET.get('search', None) !="" and request.GET.get('search-select', None) == "":
+        
+            features = Features.objects.filter(name__icontains=request.GET['search'])
+        
+    
+        elif request.GET.get('search', None) !="" and request.GET.get('search-select', None) != "":
+        
+            features = Features.objects.filter(name__icontains=request.GET['search'], type=request.GET.get('search-select', None))
+        
+        elif request.GET.get('search', None) =="" and request.GET.get('search-select', None) != "":
+            
+            features = Features.objects.filter(type=request.GET.get('search-select', None))
+
+    
+    
+    return render(request, "features.html", {"features": features})
     
