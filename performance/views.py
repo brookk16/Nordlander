e20b1a7c17e91a380ec1533912ba1de877aa4ee1
Brook Kynaston <brookkynaston@live.fr>
2019-04-22 18:16:25 +0000

    Started setting up performance.html styling
    started performance.html styling and started adding top 5 featurs functionality to the performance view

--- performance/views.py
+++ performance/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from features.models import Features
 
 # Create your views here.
 def show_performance(request):
     """A view that displays the index page"""
+    
+    """
+    NEED TO TEST: should return top 5 features based on likes
+    """
+    top_scores = (Features.objects
+                     .order_by('-likes')
+                     .values_list('likes', flat=True)
+                     .distinct())
+    top_records = (myModel.objects
+                      .order_by('-likes')
+                      .filter(score__in=top_scores[:5]))
+    
+    
+    print(top_records)
+    
+    
+    
     return render(request, "performance.html")
