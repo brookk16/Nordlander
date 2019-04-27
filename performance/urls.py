d6bd3a11d025760057574aa30be96d4ef90d6237
Brook Kynaston <brookkynaston@live.fr>
2019-04-27 02:14:46 +0000

    Added chart.js functionality and started styling performance page
    started adding chart.je charts to performance.html page and started styling (added table for top 5's)

--- performance/urls.py
+++ performance/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import show_performance
+from .views import show_performance, graph_data
 
 urlpatterns = [
     url(r'^$', show_performance, name='show_performance'),
+    url(r'^api/data', graph_data, name='graph_data'),
 ]
--- performance/views.py
+++ performance/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from features.models import Features
 from bugs.models import Bugs
 
+from django.contrib.auth import get_user_model
+from django.db.models import Max
+from django.db import connections
+from django.db.models import Count
+from django.http import JsonResponse
+
 # Create your views here.
 def show_performance(request):
     
@@ -23,3 +29,39 @@ def show_performance(request):
     
     
     return render(request, "performance.html", {'top_features': top_features, 'top_bugs': top_bugs})
+
+
+def graph_data(request):
+    
+    User = get_user_model()
+    
+    user_count = User.objects.all().count()
+    bug_count = Bugs.objects.all().count()
+    feature_count = Features.objects.all().count()
+    bug_max = Bugs.objects.all().aggregate(Max('upvotes'))
+    feature_max = Features.objects.all().aggregate(Max('likes'))
+    
+    bugs_and_features_todo = (Bugs.objects.filter(status="To do").count()) + (Features.objects.filter(status="To do").count())
+    bugs_and_features_doing = (Bugs.objects.filter(status="Doing").count()) + (Features.objects.filter(status="Doing").count())
+    bugs_and_features_done = (Bugs.objects.filter(status="Fixed").count()) + (Features.objects.filter(status="Available").count())
+    
+    
+     
+    features_max_upvote = feature_max
+    labels1 = ["Users", "Bugs", "Features"]
+    labels2 = ["Bugs", "Features"]
+    labels3 = ["To do", "Doing", "Done"]
+    default = [user_count, bug_count, feature_count]
+    max_values = [bug_max["upvotes__max"], feature_max["likes__max"]]
+    bugs_and_feature_status = [bugs_and_features_todo, bugs_and_features_doing, bugs_and_features_done]
+    
+    
+    data={
+        "labels1": labels1,
+        "labels2": labels2,
+        "labels3": labels3,
+        "default": default,
+        "max_values": max_values,
+        "bugs_and_feature_status": bugs_and_feature_status
+    }
+    return JsonResponse(data, safe=False)
